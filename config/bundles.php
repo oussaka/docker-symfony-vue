d0290a0da694e99c58d0a28bd8dfd013d340f699
oussaka <oussaka@users.noreply.github.com>
2019-02-05 17:09:54 +0100

    packages config

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- config/webpack.conf.js
+++ config/webpack.conf.js
@@ -4,74 +4,74 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  entry: {
-    page1: './src/AppBundle/Resources/js/page1/entrypoint.js',
-    page2: './src/AppBundle/Resources/js/page2/entrypoint.js',
-  },
-  output: {
-    path: path.resolve(__dirname, '../../src/AppBundle/Resources/public'),
-    filename: 'js/[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          scss: 'style!css!sass',
-        },
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[a|c]ss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'postcss-loader',
+    entry: {
+        page1: './assets/js/page1/entrypoint.js',
+        page2: './assets/js/page2/entrypoint.js',
+    },
+    output: {
+        path: path.resolve(__dirname, '../public'),
+        filename: 'build/[name].js',
+    },
+    module: {
+        rules: [
             {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [
-                  autoprefixer({
-                    remove: false,
-                    browsers: [
-                      'last 2 versions',
-                      'Chrome >= 52',
-                      'FireFox >= 44',
-                      'Safari >= 7',
-                      'ie >= 10',
-                      'last 4 Edge versions',
-                    ],
-                  }),
-                ],
-              },
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    scss: 'style!css!sass',
+                },
             },
-            'sass-loader',
-          ],
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'postcss-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    autoprefixer({
+                                        remove: false,
+                                        browsers: [
+                                            'last 2 versions',
+                                            'Chrome >= 52',
+                                            'FireFox >= 44',
+                                            'Safari >= 7',
+                                            'ie >= 10',
+                                            'last 4 Edge versions',
+                                        ],
+                                    }),
+                                ],
+                            },
+                        },
+                        'sass-loader',
+                    ],
+                }),
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'file-loader',
+            },
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'css/style.css',
         }),
-      },
-      {
-        test: /\.(jpg|png|svg)$/,
-        loader: 'file-loader',
-      },
+        new webpack.LoaderOptionsPlugin({
+            options: { sassLoader: { includePaths: [path.resolve(__dirname, '../node_modules')] } },
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: `'${process.env.NODE_ENV}'`,
+            },
+        }),
     ],
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'css/style.css',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: { sassLoader: { includePaths: [path.resolve(__dirname, '../node_modules')] } },
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: `'${process.env.NODE_ENV}'`,
-      },
-    }),
-  ],
 };
